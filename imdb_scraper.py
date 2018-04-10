a6f9245778885025ac4ddace4356985cf7d7cd4c
Thomas Mitzka <tm@thomasmitzka.at>
2018-04-10 16:26:39 +0200

    first commit

---
+++ imdb_scraper.py
@@ -0,0 +1,152 @@
+"""Collect information on a series from the Internet Movie Database."""
+
+import json
+from urllib.parse import urljoin
+from time import sleep
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+# Define constants.
+IMDB_URL = "http://www.imdb.com"
+FILENAME = "scraped.json"
+HTML_ERROR = (
+    "\nAn important HTML code element wasn't found!\n"
+    "Check again whether you entered the correct URL."
+)
+
+
+class IMDBScraper():
+    """Scrape and export data from imdb.com."""
+
+    def __init__(self, filename):
+        """Set attributes."""
+        self.filename = filename
+        self.first_episode, self.series_title = self.get_input()
+        self.episodes = []
+
+    @staticmethod
+    def get_input():
+        """Get series title and URL of the first episode."""
+        while True:
+            # Get URL from user.
+            print("Please enter the URL of the first episode in a season.")
+            first_episode = ""
+            while not first_episode.startswith(IMDB_URL):
+                first_episode = input("\nURL: ").strip()
+                if not first_episode.startswith(IMDB_URL):
+                    print(f"The URL has to start with \"{IMDB_URL}\".")
+            # Get series title.
+            request = requests.get(first_episode).text
+            soup = BeautifulSoup(request, "html.parser")
+            # If the HTML element containing the title is not found,
+            # the program ends.
+            try:
+                titlediv = soup.find("div", class_="titleParent")
+                series_title = titlediv.find("a")["title"]
+            except AttributeError:
+                print(HTML_ERROR)
+                sys.exit()
+            # Check to make sure we have the series the user wants.
+            print(f"\nDo you want to scrape \"{series_title}\"?")
+            answer = ""
+            while answer not in ("yn") or len(answer) != 1:
+                answer = input("Your answer (y/n): ").lower()
+            if answer == "y":
+                break
+            else:
+                print()
+        return first_episode, series_title
+
+    def scrape(self):
+        """Scrape information on all episodes from every season.
+
+        Scraped data for every episode:
+        - episode title
+        - episode number
+        - season number
+        - IMDB rating
+
+        The series title is included as well.
+        """
+        url = self.first_episode
+        print("\nPlease wait while episodes are scraped.\n")
+
+        while url:
+            request = requests.get(url).text
+            soup = BeautifulSoup(request, "html.parser")
+            # Create a dictionary to store episode data.
+            episode = {}
+
+            try:
+                # Scrape header div with all revelant information.
+                headerdiv = soup.find("div", class_="vital")
+                # Get string containing broadcast date.
+                # If the episode was not breadcast yet, skip it.
+                bc_str = headerdiv.find("a", title="See more release dates")
+            except AttributeError:
+                print(HTML_ERROR)
+                sys.exit()
+            else:
+                if "Episode airs" in bc_str.text:
+                    url = ""
+                    continue
+
+            # Get episode title.
+            episode_title = headerdiv.find("h1", itemprop="name").text.strip()
+            episode["title"] = episode_title
+
+            # Get episode and season number.
+            se_ep = headerdiv.find("div", class_="bp_heading").text.split("|")
+            season_number = se_ep[0].strip().split(" ")[-1]
+            episode["season"] = int(season_number)
+            episode_number = se_ep[1].strip().split(" ")[-1]
+            episode["episode"] = int(episode_number)
+
+            # Get IMDB rating.
+            # For some episodes, IMDB shows no rating yet.
+            # In that case, set rating to zero and show a message.
+            try:
+                rating = headerdiv.find("span", itemprop="ratingValue").text
+            except AttributeError:
+                rating = 0
+            episode["rating"] = float(rating)
+
+            # Append episode dictionary to episodes list.
+            self.episodes.append(episode)
+            # Print current episode.
+            print(f"- \"{episode_title}\"", end=" ")
+            print(f"(S{season_number} - E{episode_number})", end=" ")
+            if rating:
+                print()
+            else:
+                print("[no rating yet]")
+
+            # Sleep for a moment to reduce server load.
+            sleep(1)
+
+            # Try to get URL of next episode.
+            # If there is no URL, this is the last episode of the series.
+            try:
+                urlpart = headerdiv.find("a", class_="bp_item np_next")["href"]
+            except TypeError:
+                url = ""
+            else:
+                url = urljoin(IMDB_URL, urlpart)
+
+    def store(self):
+        """Store scraped information in a JSON file."""
+        with open(self.filename, "w") as json_file:
+            json.dump((self.series_title, self.episodes), json_file)
+            print(f"\nScraped data was saved in the file {self.filename}.")
+
+
+def main():
+    """Call methods to scrape information and store it."""
+    imdbs = IMDBScraper(FILENAME)
+    imdbs.scrape()
+    imdbs.store()
+
+if __name__ == "__main__":
+    main()
---
+++ ratings_plotter.py
@@ -0,0 +1,145 @@
+"""Plot season ratings from IMDB, based on scraped data."""
+
+import os
+import sys
+import json
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import imdb_scraper
+
+# Get filename from scraper module.
+# This ensures that we're looking for the right file to open.
+FILENAME = imdb_scraper.FILENAME
+
+
+class SeasonRatingsPlotter():
+    """A class to create a graph of season ratings."""
+
+    def __init__(self, filename):
+        """Initialize attributes."""
+        self.filename = filename
+        self.series_title = ""
+        # Define list for episode data.
+        self.episodes = []
+        # Define list for numbers of all scraped seasons.
+        self.seasons = []
+
+    def import_data(self):
+        """Read scraped information from JSON file."""
+        try:
+            with open(self.filename) as json_file:
+                series_title, episodes = json.load(json_file)
+        except FileNotFoundError:
+            print(f"ERROR: {self.filename} was not found!")
+            sys.exit()
+
+        first_season = episodes[0]["season"]
+        last_season = episodes[-1]["season"]
+        seasons = list(range(first_season, last_season+1))
+
+        print(f"Data for \"{series_title}\"", end=" ")
+        if len(seasons) == 1:
+            print(f"(season {seasons[0]}) imported.\n")
+        else:
+            print(f"(seasons {seasons[0]}-{seasons[-1]}) imported.\n")
+        return series_title, episodes, seasons
+
+    def get_season_range(self):
+        """Return season range for plot."""
+        if len(self.seasons) == 1:
+            season_range = range(self.seasons[0], self.seasons[0]+1)
+        elif len(self.seasons) == 2:
+            season_range = range(self.seasons[0], self.seasons[1]+1)
+        else:
+            # Ask user for min and max season to plot.
+            print("\nPlease choose which seasons you want to include.\n")
+            min_max_seasons = []
+            required = ("first", "last")
+            for req in required:
+                season = ""
+                while (
+                        not season.isdigit() or
+                        int(season) not in self.seasons
+                ):
+                    season = input(f"{req.title()} season number: ")
+                    if not season.isdigit() or int(season) not in self.seasons:
+                        print("Enter a number from", end=" ")
+                        print(f"{self.seasons[0]} to {self.seasons[-1]}.\n")
+                min_max_seasons.append(int(season))
+                print()
+            # Sort list, just in case the higher number
+            # was entered first.
+            min_max_seasons.sort()
+            season_range = range(min_max_seasons[0], min_max_seasons[1]+1)
+        return season_range
+
+    def create_plot(self, season_range):
+        """Create plot for season ratings."""
+        # Use seaborn library to make the plot prettier.
+        sns.set_style("darkgrid")
+        colors = sns.color_palette("hls", len(season_range))
+
+        for number in season_range:
+            season = [eps for eps in self.episodes if eps["season"] == number]
+            episode_numbers = [eps["episode"] for eps in season]
+            season_ratings = [eps["rating"] for eps in season]
+            plt.plot(
+                episode_numbers,
+                season_ratings,
+                label=f"Season {number}",
+                linewidth=3,
+                color=colors.pop()
+            )
+
+        title = f"IMDB ratings for \"{self.series_title}\""
+        plt.title(title)
+        plt.xlabel("Episodes per season")
+        plt.ylabel("Rating (1-10)")
+        plt.legend()
+        plt.show()
+
+    def plot_ratings(self):
+        """Call functions to scrape, store, import, and plot data."""
+        print(
+            "This program will create a graph to compare seasons "
+            "of a series,\nbased on scraped data from the Internet "
+            "Movie Database (IMDB).\n")
+
+        scrape = False
+        while True:
+            # Scrape from IMDB if flag is set to True
+            # or if data file isn't found.
+            if scrape or not os.path.isfile(self.filename):
+                imdb_scraper.main()
+                scrape = False
+
+            # Import scraped data.
+            self.series_title, self.episodes, self.seasons = self.import_data()
+
+            # Ask the user to confirm.
+            print("Do you want to continue?")
+            print(f"Enter \"y\" to show a graph for \"{self.series_title}\".")
+            print("Enter \"n\" to scrape a different series instead.\n")
+            answer = ""
+            while answer not in "yn" or len(answer) != 1:
+                answer = input("Your answer (y/n): ").lower()
+            if answer == "n":
+                print()
+                scrape = True
+                continue
+
+            # Get season range and create plot.
+            season_range = self.get_season_range()
+            self.create_plot(season_range)
+            break
+
+
+def main(filename):
+    """The main function."""
+    srp = SeasonRatingsPlotter(filename)
+    srp.plot_ratings()
+
+if __name__ == "__main__":
+    main(FILENAME)
